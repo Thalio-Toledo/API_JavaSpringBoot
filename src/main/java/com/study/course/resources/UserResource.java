2883b0bae87e28950ac608ac4d348d72ee79ed3d
Thalio-Toledo <thalio.dev@gmail.com>
2025-03-12 17:26:43 -0300

    User Service and Repository

--- src/main/java/com/study/course/resources/UserResource.java
+++ src/main/java/com/study/course/resources/UserResource.java
@@ -1,21 +1,35 @@
 package com.study.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.course.entities.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.study.course.services.UserService;
 
 
 @RestController
 @RequestMapping(value ="/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L,"Maria", "maria@gmail.com", "99999999","12345");
+	public ResponseEntity<List<User>> findAll(){
+		var users = service.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		var user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 }
+
