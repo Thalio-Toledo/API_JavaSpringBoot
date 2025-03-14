0bbbc424aab0550cedc7c148666c83bde351e55f
Thalio-Toledo <thalio.dev@gmail.com>
2025-03-14 18:22:40 -0300

    Added Enum OrderStatus

--- src/main/java/com/study/course/services/OrderService.java
+++ src/main/java/com/study/course/services/OrderService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.study.course.entities.Order;
-import com.study.course.entities.User;
 import com.study.course.repositories.OrderRepository;
-import com.study.course.repositories.UserRepository;
 
 @Service
 public class OrderService {
